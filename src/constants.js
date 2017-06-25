b35c1929262f65165b7eafadabfe5d9c96f10f23
Nikolas Burk <nikolas.burk@gmail.com>
2017-06-25 18:24:03 +0200

    minor improvements in styling

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 export const GC_USER_ID = 'graphcool-user-id'
-// export const GC_USER_NAME = 'graphcool-user-name'
 export const GC_AUTH_TOKEN = 'graphcool-auth-token'
 
 export const LINKS_PER_PAGE = 5
