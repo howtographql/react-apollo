01feb3d4f867f9b1e15bf7582912ddbf7f33ee32
Nikolas Burk <nikolas.burk@gmail.com>
2017-07-31 16:05:26 +0200

    removed project id

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-trans
 import { GC_AUTH_TOKEN } from './constants'
 
 const networkInterface = createNetworkInterface({
-  uri: 'https://api.graph.cool/simple/v1/cj5s6lcr2e1hz0160w8uapdgo'
+  uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'
 })
 
-const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj5s6lcr2e1hz0160w8uapdgo', {
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/__PROJECT_ID__', {
   reconnect: true,
   connectionParams: {
     authToken: localStorage.getItem(GC_AUTH_TOKEN),
