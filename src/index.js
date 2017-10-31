0003b7dd5e61bdf3f63e28dd7a432926e57b61f9
Nikolas Burk <nikolas.burk@gmail.com>
2017-10-31 16:31:13 +0100

    updated react dependencies

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-trans
 import { GC_AUTH_TOKEN } from './constants'
 
 const networkInterface = createNetworkInterface({
-  uri: 'https://api.graph.cool/simple/v1/__PROJECT_ID__'
+  uri: 'https://api.graph.cool/simple/v1/__SERVICE_ID__'
 })
 
-const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/__PROJECT_ID__', {
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/__SERVICE_ID__', {
   reconnect: true,
   connectionParams: {
     authToken: localStorage.getItem(GC_AUTH_TOKEN),
