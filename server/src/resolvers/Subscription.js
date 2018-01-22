f8c88c13caf55e43579e738556bdd4d75fbb9300
Nikolas <nikolas.burk@gmail.com>
2018-01-22 18:10:27 +0100

    Update Subscription.js

--- server/src/resolvers/Subscription.js
+++ server/src/resolvers/Subscription.js
@@ -1,13 +1,11 @@
 const newLink = {
   subscribe: (parent, args, ctx, info) => {
-    console.log(`new link resolver`)
     return ctx.db.subscription.link({}, info)
   },
 }
 
 const newVote = {
   subscribe: (parent, args, ctx, info) => {
-    console.log(`new vote resolver`)
     return ctx.db.subscription.vote({}, info)
   },
 }
