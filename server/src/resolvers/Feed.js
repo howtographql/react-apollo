8952532356218ab0ed9f92b7119f980a5658b2cf
Carlos Rufo Jimenez <carlosrj@macbookcarlosrj.local>
2018-03-03 15:30:53 +0100

    Initial commit

--- server/src/resolvers/Feed.js
+++
@@ -1,8 +0,0 @@
-function links(parent, args, context, info) {
-  const { linkIds } = parent
-  return context.db.query.links({ where: { id_in: linkIds } }, info)
-}
-
-module.exports = {
-  links,
-}
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const AUTH_TOKEN = 'auth-token'
-export const LINKS_PER_PAGE = 5
