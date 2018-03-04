9ae7455d641262e03e80078980a8241e75023015
Carlos Rufo Jimenez <carlosrj@macbookcarlosrj.local>
2018-03-04 13:36:01 +0100

    3.3-end-mutations-v2.1

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,47 +1,6 @@
 import React, { Component } from 'react'
-import { graphql } from 'react-apollo'
+import { Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
-
-class CreateLink extends Component {
-  state = {
-    description: '',
-    url: '',
-  }
-
-  _createLink = async () => {
-    const { description, url } = this.state
-    await this.props.postMutation({
-      variables: {
-        description,
-        url
-      }
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="flex flex-column mt3">
-          <input
-            className="mb2"
-            value={this.state.description}
-            onChange={e => this.setState({ description: e.target.value })}
-            type="text"
-            placeholder="A description for the link"
-          />
-          <input
-            className="mb2"
-            value={this.state.url}
-            onChange={e => this.setState({ url: e.target.value })}
-            type="text"
-            placeholder="The URL for the link"
-          />
-        </div>
-        <button onClick={() => this._createLink()}>Submit</button>
-      </div>
-    )
-  }
-}
 
 // 1
 const POST_MUTATION = gql`
@@ -56,5 +15,39 @@ const POST_MUTATION = gql`
   }
 `
 
+class CreateLink extends Component {
+  state = {
+    description: '',
+    url: '',
+  }
+
+  render() {
+    const { description, url } = this.state
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            className="mb2"
+            value={description}
+            onChange={e => this.setState({ description: e.target.value })}
+            type="text"
+            placeholder="A description for the link"
+          />
+          <input
+            className="mb2"
+            value={url}
+            onChange={e => this.setState({ url: e.target.value })}
+            type="text"
+            placeholder="The URL for the link"
+          />
+        </div>
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+          {postMutation => <button onClick={postMutation}>Submit</button>}
+        </Mutation>
+      </div>
+    )
+  }
+}
+
 // 3
-export default graphql(POST_MUTATION, { name: 'postMutation' })(CreateLink)
+export default CreateLink;
