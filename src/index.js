26ccb456d0640bf1d1fd5b0d5fcfcb1235a667c2
Carlos Rufo Jimenez <software@carlosrj.com>
2018-04-01 13:56:06 +0200

    5.4-end-authentication-v2.1-tmp

--- src/index.js
+++ src/index.js
@@ -5,35 +5,24 @@ import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
+import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { BrowserRouter } from 'react-router-dom'
 import { AUTH_TOKEN } from './constants'
-import { ApolloLink } from 'apollo-client-preset'
 import { setContext } from 'apollo-link-context'
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' })
-// const authLink = setContext((_, { headers }) => {
-//   // get the authentication token from local storage if it exists
-//   const token = localStorage.getItem('token')
-//   // return the headers to the context so httpLink can read them
-//   return {
-//     headers: {
-//       ...headers,
-//       authorization: token ? `Bearer ${token}` : ''
-//     }
-//   }
-// })
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+})
 
-const authLink = new ApolloLink((operation, forward) => {
+const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem(AUTH_TOKEN)
-  const authorizationHeader = token ? `Bearer ${token}` : null
-  operation.setContext({
+  return {
     headers: {
-      authorization: authorizationHeader
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
     }
-  })
-  return forward(operation)
+  }
 })
 
 const client = new ApolloClient({
