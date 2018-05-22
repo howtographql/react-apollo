0282dd9f8410d753a06f49b98a35b37e5ce0d76a
Kyle Montag <thekylemontag@gmail.com>
2018-05-22 15:48:18 -0400

    fixes #42

--- server/src/resolvers/Feed.js
+++ server/src/resolvers/Feed.js
@@ -1,6 +1,7 @@
 function links(parent, args, context, info) {
-  const { linkIds } = parent
-  return context.db.query.links({ where: { id_in: linkIds } }, info)
+  const { linkIds, orderBy } = parent
+  const where = { id_in: linkIds }
+  return context.db.query.links({ where, orderBy }, info)
 }
 
 module.exports = {
