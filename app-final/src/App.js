c6386e8457bdb2795d4981d5b375a5b3ab84c43e
Can Göktas <can.goektas@gmail.com>
2018-12-03 21:59:12 +0100

    Use App.js from the tutorial

--- app-final/src/App.js
+++ app-final/src/App.js
@@ -5,57 +5,60 @@ import Query from "./Query";
 import Mutation from "./Mutation";
 
 class App extends Component {
-  state = {
-    count: "3"
-  };
+  state = { count: "3" };
 
   handleCountChange = e => {
     this.setState({ count: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.fetchData();
   };
 
   render() {
     return (
       <Query
         query={`
-            query($count: Int) {
-              todoItems(first: $count) {
-                id
-                text
-                completed
-              }
+          query($count: Int) {
+            todoItems(first: $count) {
+              id
+              text
+              completed
             }
-          `}
+          }
+        `}
         variables={{
           count: parseInt(this.state.count)
         }}
       >
-        {({ refetch, loading, error, data }) => {
-          return (
-            <div className="app">
-              <form
-                className="count-form"
-                onSubmit={e => {
-                  e.preventDefault();
+        {({ refetch, loading, error, data }) => (
+          <div className="app">
+            <form
+              className="count-form"
+              onSubmit={e => {
+                e.preventDefault();
 
-                  refetch();
-                }}
-              >
-                <input
-                  type="number"
-                  value={this.state.count}
-                  onChange={this.handleCountChange}
-                />
-                <button type="submit">Submit</button>
-              </form>
-              <div className="item-list-container">
-                <h1>To-do items:</h1>
-                {error && <p>{error.message}</p>}
-                {loading && <p>Loading...</p>}
-                {data && (
-                  <ul className="item-list">
-                    {data.todoItems.map(({ id, text, completed }) => (
-                      <Mutation
-                        mutation={`
+                refetch();
+              }}
+            >
+              <input
+                type="number"
+                value={this.state.count}
+                onChange={this.handleCountChange}
+              />
+              <button type="submit">Submit</button>
+            </form>
+            <div className="item-list-container">
+              <h1>To-do items:</h1>
+              {error && <p>{error.message}</p>}
+              {loading && <p>Loading...</p>}
+              {data && (
+                <ul className="item-list">
+                  {data.todoItems.map(todoItem => (
+                    <Mutation
+                      mutation={`
                         mutation(
                           $where: TodoItemWhereUniqueInput!
                           $data: TodoItemUpdateInput!
@@ -67,40 +70,37 @@ class App extends Component {
                           }
                         }
                       `}
-                        key={id}
-                      >
-                        {({ mutate, data }) => {
-                          const mutationResultExists = Boolean(data);
-                          const completedState = mutationResultExists
-                            ? data.updateTodoItem.completed
-                            : completed;
+                      key={todoItem.id}
+                    >
+                      {({ mutate, data }) => {
+                        const mutationResultExists = Boolean(data);
+                        const completed = mutationResultExists
+                          ? data.updateTodoItem.completed
+                          : todoItem.completed;
 
-                          return (
-                            <li
-                              className={
-                                completedState
-                                  ? "item-complete"
-                                  : "item-incomplete"
-                              }
-                              onClick={() =>
-                                mutate({
-                                  where: { id },
-                                  data: { completed: !completedState }
-                                })
-                              }
-                            >
-                              {text}
-                            </li>
-                          );
-                        }}
-                      </Mutation>
-                    ))}
-                  </ul>
-                )}
-              </div>
+                        return (
+                          <li
+                            className={
+                              completed ? "item-complete" : "item-incomplete"
+                            }
+                            onClick={() =>
+                              mutate({
+                                where: { id: todoItem.id },
+                                data: { completed: !completed }
+                              })
+                            }
+                          >
+                            {todoItem.text}
+                          </li>
+                        );
+                      }}
+                    </Mutation>
+                  ))}
+                </ul>
+              )}
             </div>
-          );
-        }}
+          </div>
+        )}
       </Query>
     );
   }
