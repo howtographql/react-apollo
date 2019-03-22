1939dfbf33ab907a0ab4d78f9a33f8f9cc13314a
Luca Lischetti <luca@sirlisko.com>
2019-03-22 14:24:12 +0000

    Fixed post mutation
    The current mutation was allowing unlogged users to post.

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -3,10 +3,12 @@ const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
 function post(parent, args, context) {
+  const userId = getUserId(context);
   return context.prisma.createLink({
     url: args.url,
     description: args.description,
-  })
+    postedBy: { connect: { id: userId } }
+  });
 }
 
 async function signup(parent, args, context) {
