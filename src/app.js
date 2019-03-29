3c5aa9b3c552639f1909e4ac7ec2f3bb698e6504
unknown <zwhitchcox@gmail.com>
2019-03-29 12:29:14 -0700

    react-router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { Form } from './sample/form'
 
 export function App() {
@@ -7,6 +8,43 @@ export function App() {
     <h1>
       Eggshell
     </h1>
-    <Form />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about" component={About} />
+        <Route path="/form" component={FormPage} />
+        
+      </div>
+    </Router>
   </div>
 }
+
+function Index() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function FormPage() {
+  return <>
+    <h2>Form</h2>
+    <Form />
+    </>
+}
+
