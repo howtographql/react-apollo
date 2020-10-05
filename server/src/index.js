2ebb56de4f136d7be55b830fe214af63ba65de80
Ryan Chenkie <ryanchenkie@gmail.com>
2020-10-05 16:59:19 -0400

    add new prisma model

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,19 @@
-const { GraphQLServer } = require('graphql-yoga')
-const { prisma } = require('./generated/prisma-client')
-const Query = require('./resolvers/Query')
-const Mutation = require('./resolvers/Mutation')
-const Subscription = require('./resolvers/Subscription')
-const User = require('./resolvers/User')
-const Link = require('./resolvers/Link')
-const Vote = require('./resolvers/Vote')
+import { ApolloServer } from 'apollo-server';
+import { PrismaClient } from '@prisma/client';
+import * as resolvers from './resolvers';
 
-const resolvers = {
-  Query,
-  Mutation,
-  Subscription,
-  User,
-  Link,
-  Vote,
-}
+const prisma = new PrismaClient();
 
-const server = new GraphQLServer({
+const server = new ApolloServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: request => ({
-    ...request,
-    prisma,
-  }),
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+  context: () => ({
+    prisma
+  })
+});
+
+server.listen().then(({ port }) => {
+  console.log(
+    `Server listening on http://localhost:${port}`
+  );
+});
---
+++ server/src/resolvers/index.js
@@ -0,0 +1,6 @@
+export * as Link from './Link';
+export * as Mutation from './Mutation';
+export * as Query from './Query';
+export * as Subscription from './Subscription';
+export * as User from './User';
+export * as Vote from './Vote';
